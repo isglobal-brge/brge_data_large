b0e680464ea4674346e5561e8453e4bcffa87f07
Carles Hernandez-Ferrer <kuragari.ch@gmail.com>
2017-09-22 18:38:17 +0200

    Removed methy samples. Updated BRGEdata.R.

--- R/BRGEdata.R
+++ R/BRGEdata.R
@@ -1,3 +1,9 @@
+#' BRGEdata
 #'
+#' @docType package
+#' @name BRGEdata
+#'
+#' @importClassesFrom SummarizedExperiment SummarizedExperiment
 #' @importClassesFrom Biobase eSet ExpressionSet
 #' @importClassesFrom rexposome ExposomeSet
+NULL
